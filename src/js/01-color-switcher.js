812ec5672ad6ecac83b4b294dbffff96786bcc65
pdubovskyi <petro.dubovskyi@gmail.com>
2022-11-27 19:55:42 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn(e) {
+  timerId = setInterval(() => {
+    const getRandomColor = getRandomHexColor();
+    document.body.style.backgroundColor = getRandomColor;
+    console.log(getRandomColor);
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function onStopBtn(e) {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
+
+console.log(startBtn);
+console.log(stopBtn);
