c0dea5ff25ea9f65349e6df7dc43f8f6655d17ca
pdubovskyi <petro.dubovskyi@gmail.com>
2022-11-30 21:33:02 +0200

    update task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,38 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+require('flatpickr/dist/themes/dark.css');
 
 const startBtn = document.querySelector('button[data-start]');
+const data = document.querySelector('#datetime-picker');
+let days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
 
-startBtn.addEventListener('click', onBtn);
-function onBtn(e) {}
+console.log(seconds.textContent);
+
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
+
+const convertMs = ms => {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+};
 
 const options = {
   enableTime: true,
@@ -12,15 +40,27 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    if (selectedDates[0].getTime() < options.defaultDate.getTime()) {
+    if (selectedDates[0] < Date.now()) {
       window.alert('Please choose a date in the future');
       startBtn.disabled = true;
     } else {
       startBtn.disabled = false;
-      console.log('Start!');
+
+      startBtn.addEventListener('click', onBtn);
+
+      function onBtn(e) {
+        timerId = setInterval(() => {
+          startBtn.disabled = true;
+          const deltaTime = selectedDates[0] - Date.now();
+          const timeComponent = convertMs(deltaTime);
+          days.textContent = timeComponent.days;
+          hours.textContent = timeComponent.hours;
+          minutes.textContent = timeComponent.minutes;
+          seconds.textContent = timeComponent.seconds;
+        }, 1000);
+      }
     }
   },
 };
 
-flatpickr('#datetime-picker', options);
+const instance = flatpickr('#datetime-picker', options);
