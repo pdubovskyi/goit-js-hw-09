070dfae3710ebfe7c679ca3e77f3aaf0a01def7a
pdubovskyi <107887827+pdubovskyi@users.noreply.github.com>
2022-12-05 20:24:37 +0000

    Deploying to gh-pages from @ pdubovskyi/goit-js-hw-09@bb8cbec06d10ab1c90dcc08e7ba471e3dfb5dc58 🚀

--- 01-color-switcher.1fcae6d2.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");t.addEventListener("click",(function(o){timerId=setInterval((function(){t.disabled=!0;var o="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=o,console.log(o)}),1e3)})),o.addEventListener("click",(function(o){clearInterval(timerId),t.disabled=!1})),console.log(t),console.log(o)}();
-//# sourceMappingURL=01-color-switcher.1fcae6d2.js.map
